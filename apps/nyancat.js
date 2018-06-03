e59212ba36cf5153a31545f2ba8a0682743b3b77
vincens2005 <bokheadpictures@gmail.com>
2018-06-03 19:01:38 +0000

    Update nyancat.js

--- apps/nyancat.js
+++ apps/nyancat.js
@@ -1,1 +1,3 @@
-document.body.style.background='url(http://f.cl.ly/items/0x1J350B3E3t1h1b1m2r/0002.gif)';soundManager.stop('omfgdogs');soundManager.createSound({id: 'omfgnyan',url: 'http://iwantaneff.in/nyan.mp3',volume: 50,autoPlay: true,loops:999});void(0);
+document.body.style.background='url(http://f.cl.ly/items/0x1J350B3E3t1h1b1m2r/0002.gif)';
+var audio = new Audio("https://vincens2005.github.io/vr/Nyan%20Cat%20[original].mp3");
+audio.play();
