d0d54ca610660fde9020e39d71163da7b3ec994b
icanthink <32580821+icanthink@users.noreply.github.com>
2018-07-01 16:04:02 -0400

    Create hamsterdance.js

---
+++ apps/hamsterdance.js
@@ -0,0 +1,3 @@
+document.body.style.background='url(http://f.cl.ly/items/0x1J350B3E3t1h1b1m2r/0002.gif)';
+var audio = new Audio("https://vincens2005.github.io/vr/Nyan%20Cat%20[original].mp3");
+audio.play();
